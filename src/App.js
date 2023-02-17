26ed39140da0ce12d952c21a22aa0c744e16011b
harishkumarforever <harishking809@gmail.com>
2023-02-17 16:59:04 +0530

    small chnages added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import "antd/dist/reset.css";
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
